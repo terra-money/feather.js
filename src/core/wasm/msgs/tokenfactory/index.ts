a7f2cb9505cbbf3640bd1e1c197223b8ccc43525
emidev98 <emilian@terra.money>
2023-04-27 11:39:17 +0300

    chore: ci coverage report

--- src/core/wasm/msgs/tokenfactory/index.ts
+++ src/core/wasm/msgs/tokenfactory/index.ts
@@ -3,6 +3,11 @@ import { MsgBurn } from './MsgBurn';
 import { MsgChangeAdmin } from './MsgChangeAdmin';
 import { MsgMint } from './MsgMint';
 
+export * from './MsgCreateDenom';
+export * from './MsgBurn';
+export * from './MsgChangeAdmin';
+export * from './MsgMint';
+
 export type WasmTokenFactory =
   | MsgCreateDenom
   | MsgBurn
